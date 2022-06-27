f2fc549071704e867dee9b27f35b91fb1c242666
abran <alexandru.bran@nokia.com>
2022-06-27 18:20:01 +0300

    updated src image

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Link from 'next/link'
 export default () => (
-  <div>Hello World. <Link href='/about' as={'/about'}><a>About</a></Link></div>
+  <div>
+    <img src={'https://alexbran8.github.io/portofolio/static/media/profile_pic.dc3c7a30c7a932197518.jpg'}></img>
+     <Link href='/about' as={'/about'}><a>About</a></Link></div>
 )

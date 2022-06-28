8b27cd8bd907310cf8b3b8c3b9396c62bef3d7f7
abran <alexandru.bran@nokia.com>
2022-06-28 21:19:39 +0300

    updated seo data

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,6 @@ import Layout from "../layout/layout";
 
 
 const MyApp = ({ Component, pageProps }) => {
-    return <Layout pageTitle={"webservices"} description={"webservices.ro website"}><Component {...pageProps} /></Layout>
+    return <Layout pageTitle={"webservices.ro"} description={"webservices.ro official website"}><Component {...pageProps} /></Layout>
   }
   export default MyApp

d2e225181cdfe33b8a2be699f48bd5cfe6de6ead
abran <alexandru.bran@nokia.com>
2022-06-28 14:00:06 +0300

    favicon test#1

--- layout/layout.js
+++ layout/layout.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import favicon from "../public/logo_small.svg"
+
 
 const Layout = ({ children, pageTitle, description, ...props }) => {
     return (
@@ -8,6 +10,11 @@ const Layout = ({ children, pageTitle, description, ...props }) => {
                 <meta charSet="utf-8" />
                 <meta name="description" content={description} />
                 <title>{pageTitle}</title>
+                <link rel="shortcut icon" href={favicon} />
+                {/* <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" /> */}
+                {/* <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" /> */}
+                {/* <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" /> */}
+                {/* <link rel="shortcut icon" href={favicon} /> */}
             </Head>
             {/* <Header /> */}
             {children}

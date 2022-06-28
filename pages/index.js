34733eee73db565eb3cff1c4fb9cc2edf73d0c4b
abran <alexandru.bran@nokia.com>
2022-06-28 13:35:56 +0300

    logo update

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,12 @@
 import Link from 'next/link'
-import logo from "../public/logo.jpg"
-
-
+import logo from "../public/logo.svg"
 
 import Image from "next/image"
-
-const prefix = '/webservices/'
-
 
 export default () => (
   <div className="logo-container" title="test">
     <Image src={logo}  title="webservices logo" alt="webservices logo" className="logo"></Image>
+    <h5>Servicii Profesionale Web Development, Web Design, Gazduire</h5>
     {/* <Link href='/about' as={'/about'}><a>About</a></Link> */}
   </div>
 )

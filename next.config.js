46af836f79ba55b691ba8f442cdc2a141105f643
abran <alexandru.bran@nokia.com>
2022-06-28 08:57:13 +0300

    more updates

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@
 const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
+  // basePath: '/webservices',
+  // assetPrefix:'/webservices',
   images: {
     loader: 'akamai',
     path: '',
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,13 @@ import Link from 'next/link'
 import logo from "../public/logo.jpg"
 
 import Image from "next/image"
+
+const prefix = '/webservices/'
+
+
 export default () => (
   <div>
-    <Image src={logo} height={100}></Image>
+    <Image src={logo} width={200} height={100}></Image>
      <Link href='/about' as={  '/about'}><a>About</a></Link></div>
   // process.env.BACKEND_URL +
 )

b3c0aab6cb81d5d305a16abdf8836f180c6690aa
abran <alexandru.bran@nokia.com>
2022-06-28 08:38:43 +0300

    new test

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
 export default () => (
-  <div>Hello World! 222 <Link href='/about' as={  '/about'}><a>About</a></Link></div>
+  <div>Hello World! 111222333 <Link href='/about' as={  '/about'}><a>About</a></Link></div>
   // process.env.BACKEND_URL +
 )

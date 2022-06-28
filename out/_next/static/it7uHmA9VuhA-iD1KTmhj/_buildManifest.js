83b32ead619a3d5b54ccc80ae3344bb269d4998c
abran <alexandru.bran@nokia.com>
2022-06-28 08:36:55 +0300

    new deploy

--- out/_next/static/it7uHmA9VuhA-iD1KTmhj/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-23919a916ba11154.js"],"/_error":["static/chunks/pages/_error-98e801c646abf33c.js"],"/about":["static/chunks/pages/about-e976c4ab754e527a.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

48a395e95c63f1a53506f569bf6bca401d6d74c3
alexbran8 <72192472+alexbran8@users.noreply.github.com>
2022-06-28 05:36:42 +0000

    Deploying to gh-pages from  @ a95bbd4a838c4f91e57b491e8a2f7a32652af400 🚀

---
+++ _next/static/Vdg0Q3dYXf2ujns9n-pTc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-77b0f53f7439615f.js"],"/_error":["static/chunks/pages/_error-98e801c646abf33c.js"],"/about":["static/chunks/pages/about-e976c4ab754e527a.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

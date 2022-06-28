e017a2fbc9dffa4e9ab191f64223d782098d8844
abran <alexandru.bran@nokia.com>
2022-06-28 09:18:05 +0300

    css test #1

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
 import Link from 'next/link'
 import logo from "../public/logo.jpg"
 
+
+
 import Image from "next/image"
 
 const prefix = '/webservices/'
 
 
 export default () => (
-  <div>
+  <div className="logo">
     <Image src={logo} className="logo"></Image>
-     <Link href='/about' as={  '/about'}><a>About</a></Link></div>
+    <Link href='/about' as={'/about'}><a>About</a></Link>
+  </div>
   // process.env.BACKEND_URL +
 )

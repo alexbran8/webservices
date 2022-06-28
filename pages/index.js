34b2e43190ffc20115255b4c613cab4fc2afd5ef
abran <alexandru.bran@nokia.com>
2022-06-28 08:46:22 +0300

    first image test

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 import Link from 'next/link'
+import logo from "../public/logo.jpg"
+
+import Image from "next/image"
 export default () => (
-  <div>Hello World! 111222333 <Link href='/about' as={  '/about'}><a>About</a></Link></div>
+  <div>
+    <Image src={logo} height={100}></Image>
+     <Link href='/about' as={  '/about'}><a>About</a></Link></div>
   // process.env.BACKEND_URL +
 )

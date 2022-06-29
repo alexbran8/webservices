a0bec3f76ec39c20ff96fdb0b2e76f0ac62eae36
abran <alexandru.bran@nokia.com>
2022-06-29 09:49:14 +0300

    wip update text

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,10 @@ export default () => (
         <h5>Servicii Profesionale Web Development, Web Design, Gazduire</h5>
         <nav></nav>
       </div>
+      <div className="dev-env">Lucrăm la dezvoltarea acestei pagini.
+        <p>Între timp ne puteți contacta sau urmări pe pagina noastră de <a href="https://www.facebook.com/webservices.ro" target="blank">facebook</a>.
+        </p>
+      </div>
       <article></article>
       <footer>
         <a href="https://www.facebook.com/webservices.ro" target="blank">dezvoltat de webservices.ro</a>

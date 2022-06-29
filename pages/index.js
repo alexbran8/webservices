bc9d30398286b69df25016addb829c07e9f58335
abran <alexandru.bran@nokia.com>
2022-06-29 11:06:30 +0300

    h5 logo update

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default () => (
     <main>
       <div className="logo-container" title="test">
         <Image src={logo} title="webservices logo" alt="webservices logo" className="logo"></Image>
-        <h5>Servicii Profesionale Web Development, Web Design, Gazduire</h5>
+        <h5>Servicii Profesionale Web Development, Web Design, Găzduire</h5>
         <nav></nav>
       </div>
       <div className="dev-env">Lucrăm la dezvoltarea acestei pagini.

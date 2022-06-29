b7579695850573d4f2e5744b4708665a46b2b05e
abran <alexandru.bran@nokia.com>
2022-06-29 11:46:05 +0300

    refactoring layout

--- layout/layout.js
+++ layout/layout.js
@@ -10,17 +10,23 @@ const Layout = ({ children, pageTitle, description, ...props }) => {
                 <meta charSet="utf-8" />
                 <meta name="description" content={description} />
                 <title>{pageTitle}</title>
-                {/* <link rel="shortcut icon" href={favicon} /> */}
-                {/* <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" /> */}
-                {/* <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" /> */}
-                {/* <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" /> */}
-                {/* <link rel="shortcut icon" href={favicon} /> */}
             </Head>
-            {/* <Header /> */}
+
             {children}
-            {/* <Footer /> */}
+            
         </div>
     );
 }
 
 export default Layout;
+
+
+
+{/* <link rel="shortcut icon" href={favicon} /> */ }
+{/* <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" /> */ }
+{/* <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" /> */ }
+{/* <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" /> */ }
+{/* <link rel="shortcut icon" href={favicon} /> */ }
+{/* <Footer /> */ }
+
+{/* <Header /> */ }

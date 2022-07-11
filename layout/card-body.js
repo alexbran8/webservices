b721162ed01ed2f1ff4b0147466a57b41306baaa
abran <alexandru.bran@nokia.com>
2022-07-11 09:22:39 +0300

    styling updates

--- layout/card-body.js
+++ layout/card-body.js
@@ -22,7 +22,9 @@ const CardBody = () => {
                 </div>
             </div>
             <div className="grid-item">
-                <h3>SITE BASIC</h3>
+                <div className="grid-item-header">
+                    <h3>SITE BASIC</h3>
+                </div>
                 <ul>
                     <li><FontAwesomeIcon className="icon" icon={faCircleCheck} />Design modern, tema personalizata</li>
                     <li><FontAwesomeIcon className="icon" icon={faCircleCheck} />pagini dedicate fiecaror sectiuni (prezentare, produse, servicii, contact, etc.)</li>
@@ -32,9 +34,14 @@ const CardBody = () => {
                     <li><FontAwesomeIcon className="icon" icon={faCircleCheck} />SEO FRIENDLY (trafic organic / performante / site ranking)</li>
                     <li> <FontAwesomeIcon className="icon" icon={faCircleXmark} />nu necesita autentificare</li>
                 </ul>
+                <div className="grid-item-footer">
+                    <button className="button">Afla mai multe informatii</button>
+                </div>
             </div>
             <div className="grid-item">
-                <h3>SITE PREMIUM</h3>
+                <div className="grid-item-header">
+                    <h3>SITE PREMIUM</h3>
+                </div>
                 <ul>
                     <li><FontAwesomeIcon className="icon" icon={faCircleCheck} />Design modern, tema personalizata, animatii</li>
                     <li><FontAwesomeIcon className="icon" icon={faCircleCheck} />pagini dedicate fiecaror sectiuni ale site-ului (prezentare, produse, servicii, preturi, contact, etc.)</li>
@@ -44,9 +51,14 @@ const CardBody = () => {
                     <li><FontAwesomeIcon className="icon" icon={faCircleCheck} />TOP SEO PERFORMANCE (trafic organic / performante / site ranking)</li>
                     <li><FontAwesomeIcon className="icon" icon={faCircleCheck} />PWA - compatibil cu toate platformele, poate fi instalat</li>
                 </ul>
+                <div className="grid-item-footer">
+                    <button className="button">Afla mai multe informatii</button>
+                </div>
             </div>
             <div className="grid-item">
-                <h3>E-COMMERCE</h3>
+                <div className="grid-item-header">
+                    <h3>E-COMMERCE</h3>
+                </div>
 
                 <ul>
                     <li><FontAwesomeIcon className="icon" icon={faCircleCheck} />Design modern, tema personalizata</li>
@@ -55,27 +67,50 @@ const CardBody = () => {
                     <li><FontAwesomeIcon className="icon" icon={faCircleCheck} />experienta utilizator imbunatatita datorita folosirii tehnologiilor moderne de programare</li>
                     <li><FontAwesomeIcon className="icon" icon={faCircleCheck} />seo friendly (trafic organic / performante / site ranking)</li>
                 </ul>
+                <div className="grid-item-footer">
+                    <button className="button">Afla mai multe informatii</button>
+                </div>
             </div>
             <div className="grid-item">
-                <h3>STYLING</h3>
+                <div className="grid-item-header">
+                    <h3>STYLING</h3>
+                </div>
+                <div className="grid-item-footer">
+                    <button className="button">Afla mai multe informatii</button>
+                </div>
             </div>
             <div className="grid-item">
-                <h3>CONSULTANTA</h3>
+                <div className="grid-item-header">
+                    <h3>CONSULTANTA</h3>
+                </div>
+                <div className="grid-item-footer">
+                    <button className="button">Afla mai multe informatii</button>
+                </div>
             </div>
             <div className="grid-item">
-                <h3>APLICATII COMPLEXE</h3>
+                <div className="grid-item-header">
+                    <h3>APLICATII COMPLEXE</h3>
+                </div>
                 <ul>
                     <li><FontAwesomeIcon className="icon" icon={faCircleCheck} />automatizare procese</li>
                     <li><FontAwesomeIcon className="icon" icon={faCircleCheck} />digitalizare procese</li>
                     <li><FontAwesomeIcon className="icon" icon={faCircleCheck} />servicii pentru programare online</li>
                 </ul>
+                <div className="grid-item-footer">
+                    <button className="button">Afla mai multe informatii</button>
+                </div>
             </div>
             <div className="grid-item">
-                <h3>SOLUTII INFRASTRUCTURA</h3>
+                <div className="grid-item-header">
+                    <h3>SOLUTII INFRASTRUCTURA</h3>
+                </div>
                 <ul>
                     <li><FontAwesomeIcon className="icon" icon={faCircleCheck} />configurare masini virtuale</li>
                     <li><FontAwesomeIcon className="icon" icon={faCircleCheck} />migrare aplicatii in containere</li>
                 </ul>
+                <div className="grid-item-footer">
+                    <button className="button">Afla mai multe informatii</button>
+                </div>
             </div>
         </div>
     );

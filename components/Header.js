f00c6f934e7bba9d1220fda3c63091ba3f0f24b8
Bran, Alexandru <alexandru.bran@haufe-lexware.com>
2023-02-26 08:27:20 +0200

    hotfix logo

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import Image from "next/image"
 export const Header = () => {
 return (
 <div className="logo-container" title="test">
-<Image src={`${process.env.PUBLIC_URL}/logo`} title="webservices logo" alt="webservices logo" className="logo"></Image>
+<Image src="/logo.svg" title="webservices logo" alt="webservices logo" width="600" height="100" className="logo"></Image>
 <h5>Servicii Profesionale Web Development, Web Design, Găzduire</h5>
 </div>
 )}

79fa4e2a69079b11ad17c94229d7adc3f2f1e6a8
Alexandru Bran <BranA>
2023-11-30 08:44:54 +0200

    updates

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,6 @@ import "../styles/navbar.scss"
 // import "./Footer.scss"
 
 import Layout from "../layout/layout";
-import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
-import { config } from "@fortawesome/fontawesome-svg-core";
-
-
-config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 const MyApp = ({ Component, pageProps }) => {
   return <Layout pageTitle={"webservices.ro"} description={"webservices.ro official website"}>
--- pages/products.js
+++ pages/products.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CardBody from '../layout/card-body'
-import Image from "next/image"
+// import Image from "next/image"
 
 import productsData from "../data/products.json"
 
